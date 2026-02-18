d8b9d1f86797798010d66d0cc395ee3f3292a21d
FinianScott <25807449+FinianScott@users.noreply.github.com>
2026-02-18 09:17:06 -0500

    Add solution to 08
    08 didn't have code to add js function. Add code to remove elemenets.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,23 @@
 const removeFromArray = function() {
+    const allArgs = Array.from(arguments);
+    const arr = allArgs[0];
+    args = allArgs.slice(1);
+
+
+
+
+    for (let i = 0; i < arr.length; i++){
+        for (let x = 0; x <args.length; x++){
+            
+            if (arr[i] === args[x]){
+                arr.splice(i,1);
+                i--;
+                break;
+
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
