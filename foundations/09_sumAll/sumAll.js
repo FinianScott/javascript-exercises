3b8f2496405d52c1556b94d060f7670296068fe2
FinianScott <25807449+FinianScott@users.noreply.github.com>
2026-02-18 09:43:32 -0500

    Solve 09
    No js code in 09. Add code to calculate sumAll.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
 const sumAll = function() {
-
+    const args = Array.from(arguments).sort((a,b)=> a - b);
+    let sum = 0;
+    let validCheck = Number.isInteger(args[0]) && Number.isInteger(args[1]) && args[0] > 0 && args[1] > 0;
+    if (validCheck){
+        for (let i = args[0]; i <= args[1]; i++){
+            sum+= i;
+        }
+        return sum;
+    }
+    else{
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
